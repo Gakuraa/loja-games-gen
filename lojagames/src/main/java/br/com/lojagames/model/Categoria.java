614ec2aecd262f3078e0bdcb3f3f17463d1ded75
Gakuraa <gabrieliwakura924@gmail.com>
2023-12-14 12:51:39 -0300

    Commit com Criação da Classe Categoria e com seu CRUD, testes feitos no insomnia

---
+++ lojagames/src/main/java/br/com/lojagames/model/Categoria.java
@@ -0,0 +1,52 @@
+package br.com.lojagames.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+
+@Entity
+@Table(name = "tb_categoria")
+public class Categoria {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotBlank(message = "O atributo nome é obrigatório")
+	@Column(length = 50)
+	private String nome;
+	
+	@NotBlank(message = "O atributo descrição é obrigatório")
+	@Column(length=250)
+	private String descricao;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	
+	
+}
